2990c133d8630d9e1f18b00c6e5ffb21be784b34
Rodrigo Cavalcanti de Castro <rodrigo.darlane@gmail.com>
2022-03-24 16:54:37 -0300

    Update product_controller_spec.rb

--- spec/Controller/product_controller_spec.rb
+++ spec/Controller/product_controller_spec.rb
@@ -4,22 +4,20 @@ RSpec.describe Api::Products::V1Controller, type: :controller do
 
   let!(:user_one) { create(:user) }
   let!(:user_two) { create(:user) }
-  let!(:product_one) { create(:product, title: "Product 1", price: 100, user_id: user_one.id) }
-  let!(:product_two) { create(:product, title: "Product 2", price: 80, user_id: user_two.id) }
+  let!(:product_one) { create(:product, title: "Product 1", price: 100) }
+  let!(:product_two) { create(:product, title: "Product 2", price: 80) }
   let(:result) do
     {
       "items" => [
                     {
                       "id" => product_two.id,
                       "title" => product_two.title,
-                      "price" => product_two.price,
-                      "user_id" => user_two.id
+                      "price" => product_two.price
                     },
                     {
                       "id" => product_one.id,
                       "title" => product_one.title,
-                      "price" => product_one.price,
-                      "user_id" => user_one.id
+                      "price" => product_one.price
                     }
                   ]
     }
